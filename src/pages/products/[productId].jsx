37cc44c7bc9ab6a651a02cead5c9c3af5f2120df
Swarup Sapkota <empswarupcoc@gmail.com>
2024-01-31 20:16:52 +0545

    loader edited

--- src/pages/products/[productId].jsx
+++ src/pages/products/[productId].jsx
@@ -23,21 +23,21 @@ const ProductDetailPage = () => {
 
   if (!product) {
     return (
-      <div className="min-h-screen">
-        <div class="loader">
-          <span class="loader-text">loading</span>
-          <span class="load"></span>
+      <div className="min-h-screen flex items-center justify-center">
+        <div className="loader">
+          <span className="loader-text">loading</span>
+          <span className="load"></span>
         </div>
       </div>
     );
   }
 
   return (
-    <div class="bg-white py-8 min-h-screen">
-      <div class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="flex flex-col md:flex-row -mx-4">
-          <div class="md:flex-1 px-4">
-            <div class="h-[460px] rounded-lg bg-gray-300 dark:bg-gray-700 mb-4">
+    <div className="bg-gray-100 py-8 min-h-screen">
+      <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex flex-col md:flex-row -mx-4">
+          <div className="md:flex-1 px-4">
+            <div className="h-[460px] rounded-lg overflow-hidden mb-4">
               <img
                 src={product.thumbnail}
                 alt={product.title}
@@ -45,49 +45,42 @@ const ProductDetailPage = () => {
               />
             </div>
           </div>
-          <div class="md:flex-1 px-4">
-            <h2 class="text-2xl font-bold text-black mb-2">{product.title}</h2>
-            <p class="text-gray-600 dark:text-gray-300 text-sm mb-4">
-              {product.description}
-            </p>
-            <div class="flex mb-4">
-              <div class="mr-4">
-                <span class="font-bold text-gray-700 dark:text-gray-300">
-                  Price:
-                </span>
-
-                <p>
-                  <span className="text-3xl font-bold text-slate-900">
+          <div className="md:flex-1 px-4">
+            <h2 className="text-2xl font-bold text-gray-800 mb-2">
+              {product.title}
+            </h2>
+            <p className="text-gray-700 mb-4">{product.description}</p>
+            <div className="mb-4">
+              <span className="rounded bg-yellow-300 px-2.5 py-0.5 text-xs font-semibold text-gray-700">
+                Rating: {product.rating}/5
+              </span>
+            </div>
+            <div className="mb-4">
+              <span className="font-bold text-gray-800">Category:</span>
+              <span className="text-gray-700 ml-2">{product.category}</span>
+            </div>
+            <div className="mr-8 mb-4">
+              <span className="font-bold text-gray-800">Availability:</span>
+              <span className="text-gray-700 ml-2">
+                {product.stock > 0 ? "In Stock" : "Out of Stock"}
+              </span>
+            </div>
+            <div className="flex flex-wrap items-center mb-4">
+              <div className="mr-8 mb-4">
+                <span className="font-bold text-gray-800">Price:</span>
+                <p className="text-lg">
+                  <span className="font-bold text-green-600">
                     $
                     {Math.round(
                       product.price -
                         (product.price * product.discountPercentage) / 100
                     )}
                   </span>
-                  <span className="text-sm text-slate-900 line-through">
+                  <span className="text-sm text-gray-500 line-through ml-2">
                     ${product.price}
                   </span>
                 </p>
               </div>
-              <div>
-                <span class="font-bold text-gray-700 dark:text-gray-300">
-                  Availability:
-                </span>
-                <span class="text-gray-600 dark:text-gray-300">
-                  {product.stock}
-                </span>
-              </div>
-              <div>
-                <span class="font-bold text-gray-700 dark:text-gray-300">
-                  Category:
-                </span>
-                <span class="text-gray-600 dark:text-gray-300">
-                  {product.category}
-                </span>
-              </div>
-              <span className="mr-2 ml-3 rounded bg-yellow-200 px-2.5 py-0.5 text-xs font-semibold">
-                Rating: {product.rating}/5
-              </span>
             </div>
           </div>
         </div>
@@ -95,5 +88,4 @@ const ProductDetailPage = () => {
     </div>
   );
 };
-
 export default ProductDetailPage;
